0fee38aeb3515616b0d1ac5142eb7d8b26582805
Dagobert Renouf <dagobert.king@gmail.com>
2016-11-24 14:56:30 +0100

    Rebuilt dev environment
    Previously: Developping live required to launch the demo-app present in the /demo directory. It loaded react-native-masonry as a local dependency to make sure the latest version was used.
    
    Problem: npm was copying files instead of linking them, meaning any change made in /lib weren't reflected in /demo. The only was was to uninstall / reinstall the package each time (and restart the packager to see the changes).
    
    Fix: The/demo app (renamed to MasonryExample) is now part of the root directory, and therefore has direct access to the library source files without needing to require it with package.json. This makes developping the lib and previewing the changes in simulator / device work naturally. The files are excluded for npm publication so don't add to the weight of the final package.

--- demo/README.md
+++
@@ -1,9 +0,0 @@
-# Demo
-Don't forget to `npm install` before running it.
-
-# Know issues
-`yarn install` doesn't work for now, it doesn't understand the local path specified in package.json:
-
-```
-"react-native-masonry": "../"
-```
---
+++ example/index.js
@@ -0,0 +1,17 @@
+/**
+ * Masonry demo app
+ * @flow
+ */
+
+import React from 'react';
+import { Text } from 'react-native';
+import Masonry from '../lib/Masonry';
+
+const MasonryExample  = () => (
+  <Masonry>
+    <Text>Hello</Text>
+    <Text>GoodBye</Text>
+  </Masonry>
+);
+
+export default MasonryExample;
