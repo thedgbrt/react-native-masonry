bd7bbe715b89097d7025ddc6f57df14c6c8e1ff0
Dagobert Renouf <dagobert.king@gmail.com>
2016-11-25 13:37:36 +0100

    Better example case

--- example/index.js
+++ example/index.js
@@ -4,16 +4,38 @@
  */
 
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Masonry from '../lib/Masonry';
+
+const sentences = [
+  "Hello",
+  "GoodBye",
+  "How are you doing today? I'm fine thank you",
+  "Who's a good boy?"
+];
+
+let lotOfSentences = [];
+
+for (let i = 0; i < 30; i++) {
+  lotOfSentences.push(...sentences);
+}
 
 const MasonryExample  = () => (
   <Masonry columns={3}>
-    <Text>Hello</Text>
-    <Text>GoodBye</Text>
-    <Text>Hello</Text>
-    <Text>GoodBye</Text>
+    {lotOfSentences.map((text, i) => (
+      <View style={styles.cell} key={i}>
+        <Text>{text}</Text>
+      </View>
+    ))}
   </Masonry>
 );
+
+
+const styles = StyleSheet.create({
+  cell: {
+    borderColor: "black",
+    borderWidth: 1
+  }
+});
 
 export default MasonryExample;
