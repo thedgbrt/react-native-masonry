716bf94f2d6caa0cd2362ccdb3e0a4d40ee93b46
Dagobert Renouf <dagobert.king@gmail.com>
2016-11-25 17:36:54 +0100

    Cell width computation (#4)
    Determines cell width based on container size and columns number.

--- example/index.js
+++ example/index.js
@@ -4,14 +4,38 @@
  */
 
 import React from 'react';
-import { Text } from 'react-native';
-import Masonry from '../lib/Masonry';
+import { StyleSheet, Text, View } from 'react-native';
+import MasonryWrapper from '../lib/MasonryWrapper';
+
+const sentences = [
+  "Hello",
+  "GoodBye",
+  "How are you doing today? I'm fine thank you",
+  "Who's a good boy?"
+];
+
+let lotOfSentences = [];
+
+for (let i = 0; i < 30; i++) {
+  lotOfSentences.push(...sentences);
+}
 
 const MasonryExample  = () => (
-  <Masonry>
-    <Text>Hello</Text>
-    <Text>GoodBye</Text>
-  </Masonry>
+  <MasonryWrapper columns={3}>
+    {lotOfSentences.map((text, i) => (
+      <View style={styles.cell} key={i}>
+        <Text>{text}</Text>
+      </View>
+    ))}
+  </MasonryWrapper>
 );
+
+
+const styles = StyleSheet.create({
+  cell: {
+    borderColor: "black",
+    borderWidth: 1
+  }
+});
 
 export default MasonryExample;
